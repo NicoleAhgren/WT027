9a14ce3f8cf37465916538c89ee7141c83281fb2
Nicole Åhgren <na223jy@student.lnu.se>
2026-05-14 11:19:45 +0200

    klar med api och påbörjar html

---
+++ public/js/api.js
@@ -0,0 +1,93 @@
+const API_URL = 'https://api-027.onrender.com/'
+
+export async function query(queryString, variables = {}) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: queryString,
+      variables: variables,
+    }),
+  })
+  const { data, errors } = await response.json()
+  if (errors) throw new Error(errors[0].message)
+    return data
+}
+
+export async function getSongs(page = 1, limit = 20, search = "") {
+  const data = await query(`
+    query($page: Int, $limit: Int, $search: String) {
+      songs(page: $page, limit: $limit, search: $search) {
+        songs {
+          id
+          title
+          artist
+          totalStreams
+          }
+          totalPages
+          currentPage
+          }
+  }  `,
+    { page, limit, search },)
+  return data.songs
+}
+
+export async function getSongById(id) {
+  const data = await query(`
+    query($id: ID!) {
+      song(id: $id) {
+        id
+        title
+        artist
+        totalStreams
+        }
+  }  `,
+    { id },)
+  return data.song
+}
+
+export async function getArtists(page = 1, limit = 20 ) {
+  const data = await query(`
+    query($page: Int, $limit: Int) {
+      artists(page: $page, limit: $limit) {
+        name
+        totalStreams
+      }
+    }
+  `, { page, limit})
+  return data.artists
+}
+
+export async function getArtistByName(name) {
+  const data = await query(`
+    query($name: String!) {
+      artist(name: $name) {
+        name
+        numberOfSongs
+        totalStreams
+        songs {
+          id
+          title
+          totalStreams
+        }
+      }
+    }
+  `, { name })
+  return data.artist
+}
+
+export async function getTopChart(limit = 20) {
+  const data = await query(`
+    query($limit: Int) {
+      topChart(limit: $limit) {
+        id
+        title
+        artist
+        totalStreams
+      }
+    }
+  `, { limit })
+  return data.topChart
+}
